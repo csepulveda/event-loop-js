aa8394b0426fbca088f0182db7ce1fba856d2b57
ghostnumber7 <marcogodoy@untitled.cl>
2016-11-22 20:55:24 -0300

    push

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.post('/data', require('./routes/post'))
+app.get('/', require('./routes/post'))
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,11 @@
-module.exports = (req, res) => {
-  let data = Object.assign({
+const geoip = require("geoip-native")
 
+module.exports = (req, res) => {
+  const countryData = geoip.lookup(req.body.ip)
+  let data = Object.assign({
+    countryCode: countryData.code,
+    countryName: countryData.name
   }, req.body)
+  console.log(data)
+  res.jsonp({status: 'OK'})
 }
