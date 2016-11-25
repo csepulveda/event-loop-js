13593f0249cd2cfb4af3bb71937c38216cea9332
Jéssica Schissato <jessicaschissato@gmail.com>
2016-11-25 16:18:24 -0300

    merge

--- bash/restart-app.sh
+++ bash/restart-app.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+export HOME=/root
 cd /var/www/event-loop-js/app/
-npm start
+pgrep PM2
+if [ $? == 0 ]; then
+    for i in $(./node_modules/.bin/pm2 status -m |awk '/pm2 id/ {print $NF}')
+        do 
+        ./node_modules/.bin/pm2 restart $i
+        sleep 5
+    done
+else
+    npm start
+fi
