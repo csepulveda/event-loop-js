de56f3fec186ea13b5f13450e459f64cd161ec8a
César Sepúlveda <cesar.sepulveda.b@gmail.com>
2016-11-25 12:28:46 -0300

    restart por hilos :D

--- bash/restart-app.sh
+++ bash/restart-app.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 export HOME=/root
 cd /var/www/event-loop-js/app/
-node_modules/.bin/pm2 kill
-npm start
+for i in $(./node_modules/.bin/pm2 status -m |awk '/pm2 id/ {print $NF}')
+    do 
+    ./node_modules/.bin/pm2 restart $i
+    sleep 5
+done
