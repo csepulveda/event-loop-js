9c2bb958e15dfec38aaa61c03d2b9fd274ba4066
César Sepúlveda <cesar.sepulveda.b@gmail.com>
2016-11-25 13:39:19 -0300

    fix

--- bash/restart-app.sh
+++ bash/restart-app.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 export HOME=/root
 cd /var/www/event-loop-js/app/
-for i in $(./node_modules/.bin/pm2 status -m |awk '/pm2 id/ {print $NF}')
-    do 
-    ./node_modules/.bin/pm2 restart $i
-    sleep 5
-done
+pgrep pm2
+if [ $? == 1 ] then;
+    for i in $(./node_modules/.bin/pm2 status -m |awk '/pm2 id/ {print $NF}')
+        do 
+        ./node_modules/.bin/pm2 restart $i
+        sleep 5
+    done
+else
+    npm start
+fi
