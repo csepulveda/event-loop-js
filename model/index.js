871ec80831a82ca91dddb5d54cdd6a0694b648ad
César Sepúlveda <cesar.sepulveda.b@gmail.com>
2016-11-26 16:24:38 -0300

    ip

--- model/index.js
+++ model/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 mongoose.Promise = global.Promise
 
-mongoose.connect(process.env.MONGO || 'mongodb://localhost/event-loop-js');
+mongoose.connect('mongodb://172.31.21.147/event-loop-js');
 
 const dataSchema = new Schema({
   name: String,
